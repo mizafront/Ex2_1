68991bd5de2823802823518ea5669487204447ab
Kirill Novikov <kolbasa712@yandex.ru>
2020-10-21 20:08:28 +0500

    jacoco

--- src/main/kotlin/ex2/Main.kt
+++ src/main/kotlin/ex2/Main.kt
@@ -1,92 +1,65 @@
 package ex2
 
-fun main() {
-    val action = false
-    val previouSumCard = 500_000
-    val previouSumVK = 20_000
-    println("""
-        Выберите Тип карты/счёта
-        1. Mastercard 
-        2. Maestro
-        3. Visa
-        4. Мир
-        5. VK PAY
-    """.trimIndent())
-    val choice = readLine()!!.toInt()
-    println("Введите сумму перевода ")
-    val sum = readLine()!!.toInt()
 
-    when (choice) {
-        in 1..2 -> firstCard(sum, action, previouSumCard)
-        in 3..4 -> secondCard(sum, previouSumCard)
-        5 -> vkPay(sum, previouSumVK)
-    }
 
-}
-
-fun firstCard(sum: Int, action: Boolean, previouSumCard: Int) {
+fun firstCard(sum: Int, action: Boolean, previouSumCard: Int) : String  {
     println("""
         От 300р до 75000р в рамках акций комиссия не взымается
         В другие дни комиссия 0.6% + 20р.
     """.trimIndent())
-
+    val massage : String
     when {
         (sum + previouSumCard < 600_000) && (sum < 150_000) -> {
             when (action) {
                 true -> {
                     if (sum in 300..75000) {
-                        println("Сумма перевода $sum р.")
+                        massage = "Сумма перевода $sum р. 1"
                     } else {
                         var amount = ((sum * 100) * 0.006 + 2000).toInt()
                         amount += sum * 100
-                        println("Сумма перевода ${amount / 100} р. ${amount % 100} кооп. ")
+                        massage = "Сумма перевода ${amount / 100} р. ${amount % 100} кооп. 2"
                     }
                 }
                 false -> {
                     var amount = ((sum * 100) * 0.006 + 2000).toInt()
                     amount += sum * 100
-                    println("Сумма перевода ${amount / 100} р. ${amount % 100} кооп. ")
+                    massage = "Сумма перевода ${amount / 100} р. ${amount % 100} кооп. 3"
                 }
             }
         }
-        else -> println("Превышен лимит отправки")
+        else -> massage = "Превышен лимит отправки"
     }
+    return massage
 
 }
 
-fun secondCard(sum: Int, previouSumCard: Int) {
+fun secondCard(sum: Int, previouSumCard: Int) : String {
     println("""
         Переводы с комиссией (минимум 35р)
     """.trimIndent())
-    var amount = sum * 100 + 3500
-
-    when {
-        (sum + previouSumCard < 600_000) && (sum < 150_000) -> {
-            when {
-                amount < 3500 -> {
-                    amount = sum * 100 + 3500
-                    println("При переводе ${sum * 100}  коп. перевод с коммисией $amount  коп.")
-                }
-                else -> {
-                    amount += sum * 100
-                    println("При переводе ${sum * 100}  коп. перевод с коммисией $amount  коп.")
-                }
-            }
+    val massage : String
+    if ((sum + previouSumCard < 600_000) && (sum < 150_000)) {
+        var amount : Int = (sum * 100 * 0.075).toInt()
+        if (amount < 3500) {
+            amount = sum * 100 + 3500
+            massage = "При переводе ${sum * 100}  коп. перевод с коммисией $amount  коп.1"
+        } else {
+            amount = ((sum * 100 * 0.075) + (sum * 100)).toInt()
+            massage = "При переводе ${sum * 100}  коп. перевод с коммисией $amount  коп.2"
         }
-        else -> println("Превышен лимит отправки")
     }
+    else massage = "Превышен лимит отправки"
+    return massage
 }
 
-fun vkPay(sum: Int, previouSumVK: Int) {
-    when {
+fun vkPay(sum: Int, previouSumVK: Int) : String {
+    val massage : String
+    massage = when {
         (sum + previouSumVK < 40_000) && (sum < 15_000) -> {
-            println("Сумма перевода $sum р.")
+            "Сумма перевода $sum р."
         }
-        else -> println("Превышен лимит отправки")
+        else -> "Превышен лимит отправки"
     }
+    return massage
 }
 
-fun vk1(sum: Int, previouSumCard: Int){
-    if (sum > previouSumCard) println("HI") else println("bye")
-
-}
--- src/test/kotlin/ex2/MainKtTest.kt
+++ src/test/kotlin/ex2/MainKtTest.kt
@@ -1,13 +1,84 @@
 package ex2
 
+import org.junit.Assert.*
 import org.junit.Test
 
-import org.junit.Assert.*
-
-class MainKtTest {
+class MainKtTest{
+    @Test
+    fun firstCard1() {
+        val sum = 1000
+        var amount = ((sum * 100) * 0.006 + 2000).toInt()
+        amount += sum * 100
+        val action = true
+        if(action == true) {
+            assertEquals("Сумма перевода $sum р. 1", firstCard(sum, true, 0))}
+    }
 
     @Test
-    fun vk1() {
-        assertEquals("HI", vk1(15000, 5000))
+    fun firstCard2() {
+        val sum = 100
+        var amount = ((sum * 100) * 0.006 + 2000).toInt()
+        amount += sum * 100
+        val action = true
+        if(action == true) {
+            assertEquals("Сумма перевода ${amount / 100} р. ${amount % 100} кооп. 2", firstCard(sum, true, 0))
+        }else assertEquals("Сумма перевода ${amount / 100} р. ${amount % 100} кооп. 3", firstCard(sum, false, 0))
+    }
+
+    @Test
+    fun firstCard3() {
+        val sum = 100
+        var amount = ((sum * 100) * 0.006 + 2000).toInt()
+        amount += sum * 100
+        val action = false
+        if(action == true) {
+            assertEquals("Сумма перевода ${amount / 100} р. ${amount % 100} кооп. 2", firstCard(sum, true, 0))
+        }else assertEquals("Сумма перевода ${amount / 100} р. ${amount % 100} кооп. 3", firstCard(sum, false, 0))
+    }
+
+    @Test
+    fun firstCard4() {
+        val sum = 500000
+        var amount = ((sum * 100) * 0.006 + 2000).toInt()
+        amount += sum * 100
+        assertEquals("Превышен лимит отправки", firstCard(sum, true, 200000))
+    }
+
+    @Test
+    fun secondCard1() {
+        val sum = 100
+        val amount = sum * 100 + 3500
+        assertEquals("При переводе ${sum * 100}  коп. перевод с коммисией $amount  коп.1", secondCard(sum, 0))
+    }
+
+    @Test
+    fun secondCard2() {
+        val sum = 10000
+        val amount = ((sum * 100 * 0.075) + (sum * 100)).toInt()
+        assertEquals("При переводе ${sum * 100}  коп. перевод с коммисией $amount  коп.2", secondCard(sum, 0))
+    }
+
+    @Test
+    fun secondCard3() {
+        val sum = 1000000
+        assertEquals("Превышен лимит отправки", secondCard(sum, 0))
+    }
+
+    @Test
+    fun vkPay1() {
+        val sum = 10000
+        assertEquals("Сумма перевода $sum р.", vkPay(sum, 0))
+    }
+
+    @Test
+    fun vkPay2() {
+        val previouSumVK = 5000
+        val sum = 100000
+        when {
+            (sum + previouSumVK < 40_000) && (sum < 15_000) -> {
+                assertEquals("Сумма перевода $sum р.", vkPay(sum, previouSumVK))
+            }
+            else -> assertEquals("Превышен лимит отправки", vkPay(sum, previouSumVK))
+        }
     }
 }
